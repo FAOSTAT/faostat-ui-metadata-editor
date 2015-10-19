55ce04a3344a9c81db76b0a96b33f10a9890da89
Kalimaha <guido.barbaglia@gmail.com>
2015-10-19 12:08:32 +0200

    Submodules added.

--- main.js
+++ main.js
@@ -1,39 +1,63 @@
-/*global require, document*/
-require.config({
-
-    baseUrl: 'src/js/libs',
-
-    paths: {
-
-        bootstrap: '//fenixrepo.fao.org/cdn/js/bootstrap/3.3.4/js/bootstrap.min',
-        handlebars: '//fenixrepo.fao.org/cdn/js/handlebars/2.0.0/handlebars',
-        domReady: '//fenixrepo.fao.org/cdn/js/requirejs/plugins/domready/2.0.1/domReady',
-        i18n: '//fenixrepo.fao.org/cdn/js/requirejs/plugins/i18n/2.0.4/i18n',
-        text: '//fenixrepo.fao.org/cdn/js/requirejs/plugins/text/2.0.12/text',
-
-        APPLICATION: '../application'
-    },
-
-    shim: {
-        q: {
-            deps: ['jquery'],
-            exports: 'q'
-        },
-        bootstrap: {
-            deps: ['jquery']
-        },
-        handlebars: {
-            exports: 'Handlebars'
-        }
-    }
-
-});
-
-require(['APPLICATION'], function (APPLICATION) {
+/*global require*/
+require([
+    './submodules/fenix-ui-common/js/Compiler',
+    './submodules/fenix-ui-common/js/paths',
+    './submodules/faostat-ui-tree/src/js/paths',
+    './submodules/faostat-ui-commons/js/paths'], function (Compiler, Common, Tree, FAOSTATCommons) {
 
     'use strict';
 
-    var app = new APPLICATION();
-    app.init();
+    var submodules_path = '../../submodules/',
+        commonConfig = Common,
+        treeConfig = Tree,
+        faostatCommonsConfig = FAOSTATCommons;
+
+    commonConfig.baseUrl = submodules_path + 'fenix-ui-common/js';
+    treeConfig.baseUrl = submodules_path + '/faostat-ui-tree/src/js';
+    faostatCommonsConfig.baseUrl = submodules_path + '/faostat-ui-commons/js';
+
+    Compiler.resolve([commonConfig, treeConfig, faostatCommonsConfig],
+
+        {
+            placeholders: {
+                'FENIX_CDN': '//fenixrepo.fao.org/cdn'
+            },
+
+            config: {
+
+                baseUrl: './src/js',
+
+                paths: {
+                    bootstrap: '{FENIX_CDN}/js/bootstrap/3.3.4/js/bootstrap.min',
+                    handlebars: '{FENIX_CDN}/js/handlebars/2.0.0/handlebars',
+                    domReady: '{FENIX_CDN}/js/requirejs/plugins/domready/2.0.1/domReady',
+                    i18n: '{FENIX_CDN}/js/requirejs/plugins/i18n/2.0.4/i18n',
+                    text: '{FENIX_CDN}/js/requirejs/plugins/text/2.0.12/text',
+                    faostatapiclient: '../js/FAOSTATAPIClient',
+                    application: submodules_path + '../src/js/application',
+                    html: '../html'
+                },
+
+                shim: {
+                    bootstrap: {
+                        deps: ['jquery']
+                    },
+                    handlebars: {
+                        exports: 'Handlebars'
+                    },
+                    faostatapiclient: {
+                        deps: ['jquery']
+                    }
+                }
+            }
+        });
+
+    /* Start the application. */
+    require(['application'], function (Application) {
+
+        var app = new Application();
+        app.init();
+
+    });
 
 });
--- src/js/application.js
+++ src/js/application.js
@@ -1,22 +1,50 @@
 /*global define, console*/
-define(['jquery'], function ($) {
+define(['jquery',
+        'handlebars',
+        'text!html/templates.hbs',
+        'FAOSTAT_UI_TREE'], function ($, Handlebars, templates, Tree) {
 
     'use strict';
 
     function APPLICATION() {
 
         this.CONFIG = {
-            lang: 'en'
+            lang: 'en',
+            placeholder_id: 'placeholder'
         };
 
     }
 
     APPLICATION.prototype.init = function (config) {
 
+        /* Variables. */
+        var source = $(templates).filter('#metadata_editor_structure').html(),
+            template = Handlebars.compile(source),
+            dynamic_data = {},
+            html;
+
         /* Extend default configuration. */
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
-        console.debug(this.CONFIG);
+        /* Load structure. */
+        html = template(dynamic_data);
+        $('#' + this.CONFIG.placeholder_id).html(html);
+
+        /* Load FAOSTAT tree. */
+        this.tree = new Tree();
+        this.tree.init({
+            placeholder_id: 'tree_placeholder',
+            code: 'QC',
+            callback: {
+                // Render Section
+                onTreeRendered: function (arg) {
+                    console.debug(arg);
+                },
+                onClick: function (callback) {
+                    console.debug(callback);
+                }
+            }
+        });
 
     };
 
