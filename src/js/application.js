9dbd4ed3a13fad5ef7817ec8189d4ddc3942566b
Kalimaha <guido.barbaglia@gmail.com>
2015-10-19 14:47:14 +0200

    Templates for the editor and the login.

--- src/js/application.js
+++ src/js/application.js
@@ -17,28 +17,47 @@ define(['jquery',
 
     APPLICATION.prototype.init = function (config) {
 
-        /* Variables. */
-        var source = $(templates).filter('#metadata_editor_structure').html(),
-            template = Handlebars.compile(source),
-            dynamic_data = {},
-            html;
-
         /* Extend default configuration. */
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
+
+        /* Variables. */
+        var source = $(templates).filter('#main_structure').html(),
+            template = Handlebars.compile(source),
+            dynamic_data = {
+                logged: false
+            },
+            html;
 
         /* Load structure. */
         html = template(dynamic_data);
         $('#' + this.CONFIG.placeholder_id).html(html);
 
+        /* Check whether the user is logged in, load the login otherwise. */
+        if (this.isLogged()) {
+            this.load_metadata_editor_structure();
+        } else {
+            this.load_login_structure();
+        }
+
+    };
+
+    APPLICATION.prototype.load_metadata_editor_structure = function () {
+
+        /* Load main structure. */
+        var source = $(templates).filter('#metadata_editor_structure').html(),
+            template = Handlebars.compile(source),
+            dynamic_data = {},
+            html = template(dynamic_data);
+        $('#content_placeholder').html(html);
+
         /* Load FAOSTAT tree. */
         this.tree = new Tree();
         this.tree.init({
             placeholder_id: 'tree_placeholder',
-            code: 'QC',
             callback: {
                 // Render Section
                 onTreeRendered: function (arg) {
-                    console.debug(arg);
+                    console.debug('rendered bitch');
                 },
                 onClick: function (callback) {
                     console.debug(callback);
@@ -46,6 +65,18 @@ define(['jquery',
             }
         });
 
+    };
+
+    APPLICATION.prototype.load_login_structure = function () {
+        var source = $(templates).filter('#login_structure').html(),
+            template = Handlebars.compile(source),
+            dynamic_data = {},
+            html = template(dynamic_data);
+        $('#content_placeholder').html(html);
+    };
+
+    APPLICATION.prototype.isLogged = function () {
+        return true;
     };
 
     return APPLICATION;
